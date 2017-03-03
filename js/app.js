5f8c2cc5e7d22dafadc4673a8e081f4ce2f158e6
Maxwell Karren <max@chuckwalladesign.com>
2017-03-02 18:16:51 -0700

    Styling and responsiveness

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Slick Slider
+$(document).ready(function(){
+  $('.carousel').slick({
+  	arrows: false,
+  	dots: true,
+    slidesToShow: 4,
+  	slidesToScroll: 4,
+  	// nextArrow: '<a href="#" class="prev arrow"><i class="fa fa-angle-left"></i></a>',
+  	// prevArrow: '<a href="#" class="next arrow"><i class="fa fa-angle-right"></i></a>',
+  	autoplay: true,
+  	autoplaySpeed: 2000,
+  	infinite: false,
+  	responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	      	dots: true,
+	        slidesToShow: 3,
+	        slidesToScroll: 1,
+	      }
+	    },
+	    {
+	      breakpoint: 767,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 479,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+  });
+});
